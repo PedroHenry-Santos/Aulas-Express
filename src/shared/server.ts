b466c3630d845dd2478b0b43e5baf7df14090dfd
Pedro Santos <pedrohenry.viana@gmail.com>
2022-06-12 09:24:54 -0300

    Adiciona cors

--- src/shared/server.ts
+++ src/shared/server.ts
@@ -10,6 +10,11 @@ AppDataSource.initialize().then(() => {
   const app = express();
 
   app.use(express.json());
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+  })
   app.use(Router);
   app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     
